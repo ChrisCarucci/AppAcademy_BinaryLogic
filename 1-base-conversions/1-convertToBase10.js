40cbe088eff9e7fe03746d9bc8605c392ae2517b
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-10-31 14:24:53 -0230

    Binary Logic Long Practice

---
+++ 1-base-conversions/1-convertToBase10.js
@@ -0,0 +1,49 @@
+// Convert the integers in the console.logs below to base 10:
+
+/******************************************************************************/
+
+const convertToBase10 = str => {
+  // Your code here// also can use parseInt
+  let base;
+  let chars ='0123456789abcdef'
+  if (str.startsWith('0b')) base = 2;
+  if (str.startsWith('0x')) base = 16;
+  let sum = 0;
+  for (let i = str.length - 1;  i >= 2; i--) {
+    let exp = str.length - 1 - i;
+    let els = str[i];
+    let el = Number(chars.indexOf(els));
+    sum += el*(base**exp)
+  }
+  console.log("END")
+  return sum;
+};
+
+
+
+
+
+
+
+
+/******************************************************************************/
+
+console.log(convertToBase10('0b1100')); // 12
+console.log(convertToBase10('0b0101')); // 5
+console.log(convertToBase10('0b1000')); // 8
+console.log(convertToBase10('0b0111')); // 7
+
+console.log('––––––');
+
+console.log(convertToBase10('0b10100101')); // 165
+console.log(convertToBase10('0b11111111')); // 255
+console.log(convertToBase10('0b01010101')); // 85
+console.log(convertToBase10('0b00110011')); // 51
+
+console.log('––––––');
+
+console.log(convertToBase10('0xf')); // 15
+console.log(convertToBase10('0xfa')); // 250
+console.log(convertToBase10('0x1234')); // 4660
+console.log(convertToBase10('0xc9a1')); // 51617
+console.log(convertToBase10('0xbf12')); // 48914
---
+++ 1-base-conversions/2-convertToBase2.js
@@ -0,0 +1,51 @@
+// Convert the integers in the console.logs below to base 2
+
+/******************************************************************************/
+
+const convertToBase2 = element => {
+  // Your code here 
+ 
+  let bis = '0b'
+  let rest = element
+  let maxExp2 = Math.floor(getMaxExp2(element))  
+  for (let i = 0; i <= maxExp2; i++)  {
+    let exp = 2**(maxExp2 - i);
+    if (exp <= rest) { 
+      bis += 1 
+      rest -= exp;      
+    } else {
+      bis += 0;
+    }
+    
+  }
+  return bis
+};
+function getMaxExp2(num) { // num >=1
+  let i = 0
+  while (2**i <= num) {
+    i++
+  }
+  i--
+  return i;
+}
+
+
+const convertToBase_2 = str => {
+  return '0b' + (+str).toString(2)
+}
+
+/******************************************************************************/
+
+console.log(convertToBase2(4)); // 0b100
+console.log(convertToBase2(65)); // 0b1000001
+console.log(convertToBase2(256)); // 0b100000000
+console.log(convertToBase2(123)); // 0b1111011
+console.log(convertToBase2(1000)); // 0b1111101000
+
+console.log('––––––');
+
+console.log(convertToBase2('0xf')); // 0b1111
+console.log(convertToBase2('0xfa')); // 0b11111010
+console.log(convertToBase2('0x1234')); // 0b1001000110100
+console.log(convertToBase2('0xc9a1')); // 0b1100100110100001
+console.log(convertToBase2('0xbf12')); // 0b1011111100010010
